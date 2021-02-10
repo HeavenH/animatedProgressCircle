d59b8364a9d9d71078b1c56b4d6cd79a7096dd38
Gilmar Silva <gilmarheavensilva@hotmail.com>
2021-02-10 12:44:40 -0300

    source code

---
+++ src/components/nextButton.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useRef } from "react";
+import { StyleSheet, View, TouchableOpacity, Animated } from "react-native";
+import Svg, { G, Circle } from "react-native-svg";
+
+import { AntDesign } from "@expo/vector-icons";
+
+const NextButton = ({ percentage, scrollTo }) => {
+  const size = 128;
+  const strokeWidth = 2;
+  const center = size / 2;
+  const radius = size / 2 - strokeWidth / 2;
+  const circuference = 2 * Math.PI * radius;
+
+  const progressAnimation = useRef(new Animated.Value(0)).current;
+  const progressRef = useRef(null);
+
+  const animation = (toValue) => {
+    return Animated.timing(progressAnimation, {
+      toValue,
+      duration: 250,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  useEffect(() => {
+    animation(percentage);
+  }, [percentage]);
+
+  useEffect(() => {
+    progressAnimation.addListener(
+      (value) => {
+        const strokeDashoffset =
+          circuference - (circuference * value.value) / 100;
+
+        if (progressRef?.current) {
+          progressRef.current.setNativeProps({
+            strokeDashoffset,
+          });
+        }
+      },
+      [percentage]
+    );
+    return () => {
+      progressAnimation.removeAllListeners();
+    };
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Svg width={size} height={size}>
+        <G rotation="-90" origin={center}>
+          <Circle
+            stroke="#493d8a"
+            cx={center}
+            cy={center}
+            r={radius}
+            strokeWidth={strokeWidth}
+            strokeDasharray={circuference}
+            ref={progressRef}
+          />
+        </G>
+      </Svg>
+      <TouchableOpacity
+        onPress={scrollTo}
+        style={styles.button}
+        activeOpacity={0.6}
+      >
+        <AntDesign name="arrowright" size={32} color="#fff" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  button: {
+    position: "absolute",
+    backgroundColor: "#493d8a",
+    borderRadius: 100,
+    padding: 20,
+  },
+});
+
+export default NextButton;
---
+++ src/slides.ts
@@ -0,0 +1,29 @@
+export default [
+  {
+    id: "1",
+    title: "Quick & Easy Payments",
+    description:
+      "Grow your bussiness by accepting card payments with the new card reader.",
+    image: require("../assets/imgs/img_1.png"),
+  },
+  {
+    id: "2",
+    title: "Smart Point of Sale",
+    description:
+      "Complete point of sale software tailored to your business needs.",
+    image: require("../assets/imgs/img_2.png"),
+  },
+  {
+    id: "3",
+    title: "Instant Notifications",
+    description:
+      "Instant notifications let your quilcky see new purchases and messages.",
+    image: require("../assets/imgs/img_3.png"),
+  },
+  {
+    id: "4",
+    title: "Customize Everything",
+    description: "Adjust your system to speed up your checkout.",
+    image: require("../assets/imgs/img_4.png"),
+  },
+];
